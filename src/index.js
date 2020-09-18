aa0b8bc0ba074b94da94c85ad931084dfb66e518
@wei_xuanming <17608596732@163.com>
2020-09-18 11:51:34 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
-import Data from './data.xml';
+import printMe from './print.js';
 function component() {
   var element = document.createElement("div");
+  var btn = document.createElement('button');
 
   // // Lodash, now imported by this script
   element.innerHTML = _.join(["你好", "webpack"], " ");
-  element.classList.add("hello");
 
-  // 将图像添加到我们现有的 div。
-  var myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data)
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,19 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
-      }
-    ],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "dist"),
   },
 };
